bf6666fb8c8d1b23826f8b38f46f83f928d7a67c
Zhihao Jin <37702019+jzh-Bill@users.noreply.github.com>
2024-09-21 17:58:23 -0400

    Switch block version debug
    Changed the onclick function on all buttons.

--- app/src/main/java/com/example/lab3simplecalculatorjava/MainActivity.java
+++ app/src/main/java/com/example/lab3simplecalculatorjava/MainActivity.java
@@ -14,9 +14,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private enum Operator {none, add, minus, multiply, divide}
-//    private double data1 = 0, data2 = 0;
-//    private Operator optr = Operator.none;
+    private enum Operator {none, add, sub, mul, div, eq}
+    private double data01=0, data02 = 0;
+    private Operator opp = Operator.none;
+    private boolean hasDot = false;
+    private boolean requiresCleaning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,112 +31,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-//
-//    public void btn00Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "0");
-//    }
-//
-//    public void btn01Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "1");
-//    }
-//
-//    public void btn02Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "2");
-//    }
-//    public void btn03Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "3");
-//    }
-//    public void btn04Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "4");
-//    }
-//    public void btn05Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "5");
-//    }
-//    public void btn06Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "6");
-//    }
-//    public void btn07Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "7");
-//    }
-//    public void btn08Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "8");
-//    }
-//    public void btn09Click(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + "9");
-//    }
-//    public void btnAddClick(View view) {
-//        optr = Operator.add;
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        data1 = Double.parseDouble(eText.getText().toString());
-//        eText.setText("");
-//    }
-//    public void btnMinusClick(View view) {
-//        optr = Operator.minus;
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        data1 = Double.parseDouble(eText.getText().toString());
-//        eText.setText("");
-//    }
-//    public void btnMultiplyClick(View view) {
-//        optr = Operator.multiply;
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        data1 = Double.parseDouble(eText.getText().toString());
-//        eText.setText("");
-//    }
-//    public void btnDivideClick(View view) {
-//        optr = Operator.divide;
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        data1 = Double.parseDouble(eText.getText().toString());
-//        eText.setText("");
-//    }
-//    public void btnClearClick(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText("");
-//    }
-//    public void btnDotClick(View view) {
-//        TextView eText = (TextView)findViewById(R.id.resultEdit);
-//        eText.setText(eText.getText() + ".");
-//    }
-//
-//    public void btnResultClick(View view) {
-//        if (optr != Operator.none) {
-//            TextView eText = (TextView)findViewById(R.id.resultEdit);
-//            data2 = Double.parseDouble(eText.getText().toString());
-//            double result = 0;
-//            if (optr == Operator.add) {
-//                result = data1 + data2;
-//            } else if (optr == Operator.minus) {
-//                result = data1 - data2;
-//            } else if (optr == Operator.multiply) {
-//                result = data1 * data2;
-//            } else if (optr == Operator.divide) {
-//                result = data1 / data2;
-//            }
-//
-//            optr = Operator.none;
-//            data1 = result;
-//            if ((result - (int) result) != 0) {
-//                eText.setText(String.valueOf(result));
-//            } else {
-//                eText.setText(String.valueOf((int) result));
-//            }
-//        }
-//    }
-
-    private enum Operator {none, add, sub, mul, div, eq}
-    private double data01=0, data02 = 0;
-    private Operator opp = Operator.none;
-    private boolean hasDot = false;
-    private boolean requiresCleaning = false;
 
     //Function called every time a number button is pressed
     public void onClickNumericalButton(View view) {
